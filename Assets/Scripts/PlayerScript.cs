9c1d42a5e779fab0d74a44cae428ba462a98e28c
Peter Wennerstrøm Eriksen <PWE@commentor.dk>
2019-03-28 14:37:38 +0100

    Commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,12 +3,17 @@
 public class PlayerScript : MonoBehaviour
 {
     public float Speed = 10;
+    public SpriteAnimationFrames_IdleRun Anim;
+
     Transform transform_;
+    SpriteRenderer renderer_;
     Rigidbody2D body_;
+    bool flipX_;
 
     void Awake()
     {
         transform_ = transform;
+        renderer_ = GetComponent<SpriteRenderer>();
         body_ = GetComponent<Rigidbody2D>();
     }
 
@@ -18,6 +23,15 @@ public class PlayerScript : MonoBehaviour
         var horz = Input.GetAxisRaw("Horizontal");
         var vert = Input.GetAxisRaw("Vertical");
         Vector3 vec = new Vector3(horz, vert, 0) * Speed * dt;
-        body_.MovePosition(transform_.position + vec);
+
+        bool isRunning = vec != Vector3.zero;
+        if (isRunning)
+        {
+            body_.MovePosition(transform_.position + vec);
+            flipX_ = vec.x < 0;
+        }
+
+        renderer_.sprite = SimpleSpriteAnimator.GetAnimationSprite(isRunning ? Anim.Run : Anim.Idle, Anim.DefaultAnimationFramesPerSecond);
+        renderer_.flipX = flipX_;
     }
 }
